48f01c1562f825e3462f3b7737877be19c76ba7e
Alejandro Lopez <alejandro.lopez@enti.cat>
2023-02-15 14:29:58 +0100

    Adding weapon and moves with a weapon

--- index.js
+++ index.js
@@ -1,48 +1,13 @@
 const http = require('http');
 const fs = require('fs');
+const filesStatic = require('node-static');
 
-function sendIndex (response) {
-	
-	fs.readFile("index.html", function (err, data) {
-
-		if (err) {
-			console.error(err);
-			return;
-		}
-
-		response.writeHead(200, {"Content-Type": "text/html"});
-		response.write(data);
-		response.end();
-
-	});
-}
-
-function sendPlayer (response, image) {
-	
-	fs.readFile("imgs/" + image, function (err, data) {
-
-		if (err) {
-			console.error(err);
-			return;
-		}
-
-		response.writeHead(200, {"Content-Type": "image/png"});
-		response.write(data);
-		response.end();
-	});
-}
+let files = new filesStatic.Server("./public");
 
 http.createServer(function (request, response) {
 	
-	let url = request.url.split("/");
-
-	if (url[1].match("png")) {
-
-		sendPlayer(response, url[1]);
-		return;
-	
-	}
-
-	sendIndex(response);
+	request.addListener('end', () => {
+		files.serve(request, response);
+	}).resume();
 
 }).listen(6969);
